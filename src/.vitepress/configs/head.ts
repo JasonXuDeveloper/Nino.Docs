6866fba98c4f5508a4f9ebfbf76b7912e12c40f8
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2024-07-11 00:28:16 +0800

    Update head.ts

--- src/.vitepress/configs/head.ts
+++ src/.vitepress/configs/head.ts
@@ -1,13 +1,13 @@
 import type { HeadConfig } from 'vitepress'
 
 export const head: HeadConfig[] = [
-  ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  ['meta', { name: 'theme-color', content: 'var(--vp-c-brand-1)' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
   ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
   ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
+  ['link', { rel: 'mask-icon', href: '/favicon.ico', color: 'var(--vp-c-brand-1)' }],
   ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
 ]
